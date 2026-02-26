c34b36c56ada03603f7b015ee5240a3b21d6b9b3
Mahdi Khoramabadi <mahdi.sidsid@gmail.com>
2026-02-26 14:14:07 +0330

    fix

---
+++ src/main/java/com/github/meytiii/meypacks/MeyPacks.java
@@ -0,0 +1,16 @@
+package com.github.meytiii.meypacks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MeyPacks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("MeyPacks has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("MeyPacks has been disabled!");
+    }
+}
