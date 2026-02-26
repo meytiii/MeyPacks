46e647e5fa5d1e600e318752da455fe445b16734
Mahdi Khoramabadi <mahdi.sidsid@gmail.com>
2026-02-26 14:31:48 +0330

    added storage to backpakcs

--- src/main/java/com/github/meytiii/meypacks/BackpackCommand.java
+++ src/main/java/com/github/meytiii/meypacks/BackpackCommand.java
@@ -7,7 +7,19 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
+import java.util.Map;
+import java.util.UUID;
+
 public class BackpackCommand implements CommandExecutor {
+
+    // A field to hold the map that we get from the main class
+    private final Map<UUID, Inventory> backpacks;
+
+    // The Constructor: This is called from the main class.
+    // It receives the backpacks map and stores it in our field.
+    public BackpackCommand(Map<UUID, Inventory> backpacks) {
+        this.backpacks = backpacks;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -17,8 +29,11 @@ public class BackpackCommand implements CommandExecutor {
         }
 
         Player player = (Player) sender;
+        UUID playerUUID = player.getUniqueId();
 
-        Inventory backpack = Bukkit.createInventory(player, 27, "My Portal Backpack");
+        Inventory backpack = backpacks.computeIfAbsent(playerUUID, id -> {
+            return Bukkit.createInventory(player, 27, "My Portal Backpack");
+        });
 
         player.openInventory(backpack);
 
--- src/main/java/com/github/meytiii/meypacks/MeyPacks.java
+++ src/main/java/com/github/meytiii/meypacks/MeyPacks.java
@@ -1,14 +1,21 @@
 package com.github.meytiii.meypacks;
 
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public final class MeyPacks extends JavaPlugin {
+
+    private final Map<UUID, Inventory> backpacks = new HashMap<>();
 
     @Override
     public void onEnable() {
         getLogger().info("MeyPacks has been enabled!");
 
-        this.getCommand("backpack").setExecutor(new BackpackCommand());
+        this.getCommand("backpack").setExecutor(new BackpackCommand(this.backpacks));
     }
 
     @Override
